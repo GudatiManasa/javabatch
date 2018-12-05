e1e6fdc9090344083111962fb25342db74ceafda
dhanunjay <dhanunjay@LAPTOP-G4OJPPGE>
2018-12-05 11:10:14 +0530

    new project

---
+++ src/main/java/com/testgittool/testGitTool/App.java
@@ -0,0 +1,13 @@
+package com.testgittool.testGitTool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
